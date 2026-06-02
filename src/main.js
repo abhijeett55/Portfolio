901135bc79318e194428ab5efa3e79294cfadbfe
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-06-02 18:04:21 +0530

    Foatball

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-import { createBall } from './components/foatball/ball';
 import { createGround } from './components/foatball/ground';
 import { createGoalPost } from './components/foatball/goalpost';
 import { addLights } from './components/foatball/light';
@@ -15,23 +15,49 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.set(0, 6, 15);
+camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
-
-// Add objects
-const ball = createBall();
-scene.add(ball);
 
 scene.add(createGround());
 scene.add(createGoalPost());
 
 addLights(scene);
 
+const loader = new GLTFLoader();
+
+let football;
+
+
+loader.load(
+  '/models/Foatball.glb',
+  (gltf) => {
+    console.log('MODEL LOADED');
+
+    football = gltf.scene;
+
+    football.scale.set(10, 10, 10);
+    football.position.set(0, 0, 0);
+
+    scene.add(football);
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  }
+);
+
 function animate() {
   requestAnimationFrame(animate);
+
+  if (football) {
+    football.rotation.y += 0.01;
+  }
+
   renderer.render(scene, camera);
 }
 
